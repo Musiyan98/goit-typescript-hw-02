b587498ee3be8f8812e9a3a0f5937ccb16ae0e53
DN131198MEA <evgenij.musijan@privatbank.ua>
2024-03-10 04:01:47 +0200

    baseTypes

--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -2,15 +2,21 @@
   Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
 */
 
-function showMessage(message) {
+function showMessage(message: string): void {
   console.log(message);
 }
 
-function calc(num1, num2) {
+/*
+Враховуючи відсутність контексту в прикладі вищенаведеної функції припускаємо що message це рядок, 
+адже в заданих умовах більш доцільнішим здається тип any, 
+але враховуючи що це поганий тон було прийнято рішення застрсувати певні умовності
+*/
+
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
-function customError() {
+function customError(): never {
   throw new Error('Error');
 }
 
--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,20 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum Days {
+  SUN,
+  MON,
+  TUE,
+  WED,
+  THUR,
+  FRI,
+  SAT
+}
+
+function isWeekend (day: Days): boolean {
+  if (day === Days.SUN || day === Days.SAT) {
+    return true
+  }
+  return false
+}
